b83bb1f6fa3b95fdbf122798d27f9f9847f91b88
Shad0wz93 <Shadowz9@icloud.com>
2022-11-25 12:51:31 +0100

    todo1

---
+++ banque.js
@@ -0,0 +1,24 @@
+// variable
+const compte = document.querySelector("#compte");
+const somme = document.querySelector("#somme");
+let result="";
+const boutton = document.querySelector("#boutton")
+
+//evenement du boutton
+
+boutton.removeEventListener('click',verifier)
+
+
+function verifier(){
+    console.log(somme)
+    console.log(compte)
+
+}
+
+//on vérifie les infos du compte en banque 
+
+if (compte.length == 16) {
+    result = 'valide';
+  } else {
+    result = 'non valide';
+}
